5c3b9bc037045012fa348ccc187009454c0bf14a
Guilherme Conti <guibasconti@gmail.com>
2018-02-23 13:59:19 -0300

    Create auth router

--- server/core/router.js
+++ server/core/router.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.use(bodyParser.json());
 
+const auth = require('../controllers/auth/router');
+
+router.use('/auth', auth);
+
 router.get('/', (req, res) => {
   res.status(200).json({
     data: 'Hi'
